79b43078ed3e7b3aad91760276cd29cb317eb39b
Vianney <makotodate@hotmail.fr>
2016-04-21 15:28:36 +0200

    4 too

--- src/main/java/iut/tdd/RomanNumerals.java
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -4,6 +4,8 @@ public class RomanNumerals {
 	public String convertToRoman(String arabe) {
 		String res = "";
 		int base = Integer.valueOf(arabe);
+		if(base == 0) return "0";
+		if(base == 4) return "IV";
 		for(int i = 0; i<base; i++){
 			res += "I";
 		}
